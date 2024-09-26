e92387371d26e74a182a851641360bf087591b69
RicardoUz <Ricardouzcate68@gmail.com>
2024-09-26 10:23:56 -0500

    Actualizacion

--- api/config/db_Conenection.php
+++ api/config/db_Conenection.php
@@ -5,17 +5,26 @@
     Developer: Ricardo
     */
     
-   $data_connection = "
     $servername = "localhost";
     $username   = "postgres";
-    $password   = "unicesmag";
-    $dbname     = "beta";
+    $password   = "1234";
+    $dbname     = "Beta";
     $port       = "5432";
-    "
+
+    $data_connection="
+        host     = $servername
+        user = $username
+        password = $password
+        dbname   = $dbname
+        port     = $port
+    ";
+    
     $conn = pg_connect($data_connection);
 
     if(!$conn){
         die("Connection failed: ".pg_last_error());
+    } else {
+        echo "connection success";
     }
 
     pg_close($conn);
